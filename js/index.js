983492b3581bf77d09b90dfa18fc5f33c1ac0caa
devashrlucas <devashrlucas@gmail.com>
2019-11-18 13:51:27 -0500

    Added keypress event listener that works in Chrome.

--- js/index.js
+++ js/index.js
@@ -1,26 +1,56 @@
-let drumPads = [
-    {
+let drumPads = {
+    113: {
         id: 1,
-        keyCode: 49,
-        URL: "https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Claps/295[kb]big-verby-clap.wav.mp3",
-        description: "big verby clap",
+        sound: new Audio("https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Toms/40[kb]lotom.aif.mp3"),
+        description: "low tom",
     },
-    {
+    119: {
         id: 2,
-    }
+        sound: new Audio("https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Toms/38[kb]midtom.aif.mp3"),
+        description: "mid tom",
+    },
+    101: {
+        id: 3,
+        sound: new Audio("https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Toms/35[kb]hitom.aif.mp3"),
+        description: "high tom",
+    },
+   97: {
+        id: 4,
+        sound: new Audio("https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Hats/20[kb]quickhat1.aif.mp3"),
+        description: "hihat",
+    },
+    115: {
+        id: 5,
+        sound: new Audio("https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Hats/88[kb]bigopenhh.aif.mp3"),
+        description: "open hihat",
+    },
+    100: {
+        id: 6,
+        sound: new Audio("https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Hats/240[kb]HatRoll.aif.mp3"),
+        description: "hihat roll",
+    },
+    122: {
+        id: 7,
+        sound: new Audio("https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Snares/5[kb]sidestick.aif.mp3"),
+        description: "sidestick",
+    },
+    120: {
+        id: 8,
+        sound: new Audio("https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Snares/19[kb]just_a_snare.aif.mp3"),
+        description: "snare",
+    },
+    99: {
+        id: 9,
+        sound: new Audio("https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Kicks/120[kb]basic-ekick.wav.mp3"),
+        description: "kick",
+    },
+};
 
-];
-
-let testPad = {
-    id: 1,
-    keyCode: 49,
-    sound: new Audio("https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Claps/295[kb]big-verby-clap.wav.mp3"),
-    description: "big verby clap",
+function keypressEventHandler(e) {
+    let key = e.keyCode;
+  if (e.keyCode in drumPads) {
+     drumPads[key].sound.play();
+ }
 }
 
-
-document.onkeypress = function(e) {
-if (e.keyCode == testPad.keyCode) {
-    testPad.sound.play();
-    }
-};
+document.addEventListener("keypress", keypressEventHandler, true);
