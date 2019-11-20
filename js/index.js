0f046393e0ae6781b9f0bc53a218c31de71fe571
devashrlucas <devashrlucas@gmail.com>
2019-11-20 10:51:58 -0500

    Started keypress color change effect.

--- js/index.js
+++ js/index.js
@@ -1,56 +1,66 @@
 let drumPads = {
     113: {
-        id: 1,
+        id: "pad-1",
         sound: new Audio("https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Toms/40[kb]lotom.aif.mp3"),
         description: "low tom",
     },
     119: {
-        id: 2,
+        id: "pad-2",
         sound: new Audio("https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Toms/38[kb]midtom.aif.mp3"),
         description: "mid tom",
     },
     101: {
-        id: 3,
+        id: "pad-3",
         sound: new Audio("https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Toms/35[kb]hitom.aif.mp3"),
         description: "high tom",
     },
    97: {
-        id: 4,
+       id: "pad-4",
         sound: new Audio("https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Hats/20[kb]quickhat1.aif.mp3"),
         description: "hihat",
     },
     115: {
-        id: 5,
+        id: "pad-5",
         sound: new Audio("https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Hats/88[kb]bigopenhh.aif.mp3"),
         description: "open hihat",
     },
     100: {
-        id: 6,
+        id: "pad-6",
         sound: new Audio("https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Hats/240[kb]HatRoll.aif.mp3"),
         description: "hihat roll",
     },
     122: {
-        id: 7,
+        id: "pad-7",
         sound: new Audio("https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Snares/5[kb]sidestick.aif.mp3"),
         description: "sidestick",
     },
     120: {
-        id: 8,
+        id: "pad-8",
         sound: new Audio("https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Snares/19[kb]just_a_snare.aif.mp3"),
         description: "snare",
     },
     99: {
-        id: 9,
+        id: "pad-9",
         sound: new Audio("https://sampleswap.org/samples-ghost/DRUMS%20(SINGLE%20HITS)/Kicks/120[kb]basic-ekick.wav.mp3"),
         description: "kick",
     },
 };
 
-function keypressEventHandler(e) {
+function keypressSoundEventHandler(e) {
     let key = e.keyCode;
   if (e.keyCode in drumPads) {
      drumPads[key].sound.play();
  }
 }
 
-document.addEventListener("keypress", keypressEventHandler, true);
+function keypressColorEventHandler(e) {
+    let key = e.keyCode;
+    if (e.keyCode in drumPads) {
+        let object = drumPads[key];    
+        let objectId = object.id;
+        document.getElementById(`${objectId}`).setAttribute("class", "pressed");
+    }
+}
+
+document.addEventListener("keypress", keypressSoundEventHandler, true);
+document.addEventListener("keypress", keypressColorEventHandler, true);
